7d024060cebc9424b9267883a564aa3b8eccd4de
Alexey <Leha_1989@mail.ru>
2022-12-09 13:04:37 +0300

    practika 3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@
 
 // Практика ч.1
 
-const numbersOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+let numbersOfFilms;
+
+function start() {
+    numbersOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+    while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)) {
+        numbersOfFilms = +prompt('Сколько фильмов вы уже смотрели', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numbersOfFilms,
@@ -12,59 +21,53 @@ const personalMovieDB = {
     privat: false
 };
 
-// Практика ч.2
-
-
-// 1-й вариант цикла
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов', ''),
-        b = prompt('На сколько оцените его', '');
-    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+            b = prompt('На сколько оцените его', '');
+        if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
     }
 }
 
+rememberMyFilms ();
 
-// 2-й вариант цикла
-
-// let num = 0;
-// while (num < 2) {
-//     const a = prompt('Один из последних просмотренных фильмов', ''),
-//         b = prompt('На сколько оцените его', '');
-//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         num++;
-//     }
-// }
-
-// 3-й вариант цикла
-
-// let num = 0;
-// const a = prompt('Один из последних просмотренных фильмов', ''),
-//       b = prompt('На сколько оцените его', '');
-// do {
-//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-//            personalMovieDB.movies[a] = b;
-//            num++;
-//            console.log(num);
-//         }
-//     }
-// while (num < 3);
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы класический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Ошибка');
+function detectedPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы класический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Ошибка');
+    }
 }
 
+detectedPersonalLevel ();
 
 
+// Практика ч.3
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+
+}
+
+writeYourGenres ();
+
